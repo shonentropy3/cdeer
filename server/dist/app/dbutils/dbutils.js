a4fe37463028dd38d931edea986602ca632037b0
caijiuyu <61570583@qq.com>
2022-07-14 07:16:40 +0000

    删除文件 server/dist

--- server/dist/app/dbutils/dbutils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Insert = void 0;
-const Insert = () => {
-    console.log('hh');
-};
-exports.Insert = Insert;
-//# sourceMappingURL=dbutils.js.map
