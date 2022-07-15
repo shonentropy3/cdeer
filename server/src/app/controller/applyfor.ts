aa1ed64e7186e594f73d563c8def111cec9884d4
caijiuyu <yucaijiu@gmail.com>
2022-07-15 16:10:04 +0800

    fe:增加后端报名接口

--- server/src/app/controller/applyfor.ts
+++ server/src/app/controller/applyfor.ts
@@ -1,23 +1,23 @@
 import { Body, Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { UserService } from '../service/user';
+import { ApplyforService } from '../service/applyfor';
 
 
 @Controller('applyFor')
 export class UserController {
-    constructor(private readonly marketService: UserService){}
+    constructor(private readonly applyforService: ApplyforService){}
 
     @Post('applyFor')  // 报名
     async applyFor(@Body() body: any){
-        return await this.marketService.getMyDemand(body)
+        return await this.applyforService.getMyDemand(body)
     }
 
     @Post('cancelApply')  // 取消报名
     async cancelApply(@Body() body: any){
-        return await this.marketService.getMyDemand(body)
+        return await this.applyforService.getMyDemand(body)
     }
     @Post('modifyApplySwitch')  //  报名开关
     async modifyApplySwitch(@Body() body: any){
-        return await this.marketService.getMyDemand(body)
+        return await this.applyforService.getMyDemand(body)
     }
 }
