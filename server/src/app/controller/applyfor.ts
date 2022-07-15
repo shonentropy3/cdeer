7ed5e0e55e6d03aeb0cd24c46233591091d5adb1
caijiuyu <yucaijiu@gmail.com>
2022-07-15 15:46:49 +0800

    en:合并

--- server/src/app/controller/applyfor.ts
+++ server/src/app/controller/applyfor.ts
@@ -8,5 +8,22 @@ export class ApplyforController {
     @Post('createApply')  // 发布需求
     async createProject(@Body() body: any){
         return await this.applyforService.apply(body)
+
+
+
+            @Post('applyFor')  // 报名
+            async applyFor(@Body() body: any){
+                return await this.applyforService.getMyDemand(body)
+            }
+        
+            @Post('cancelApply')  // 取消报名
+            async cancelApply(@Body() body: any){
+                return await this.applyforService.getMyDemand(body)
+            }
+            @Post('modifyApplySwitch')  //  报名开关
+            async modifyApplySwitch(@Body() body: any){
+                return await this.applyforService.getMyDemand(body)
+            
+
     }
 }
