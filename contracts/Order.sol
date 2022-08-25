ca86c5d4e35c5e39cae95ed8ee1e31046c7ce3a4
xilibi2003@163.com <xilibi2003@163.com>
2022-08-25 19:20:24 +0800

    fix

--- contracts/Order.sol
+++ contracts/Order.sol
@@ -15,11 +15,11 @@ import "hardhat/console.sol";
 
 // TODO:考虑阶段甲方确认后，修改后续阶段的问题
 contract Order is IOrder, Ownable {
-    
+
     enum OrderProgess {
         Init,
-        Staging,
-        Staged,
+        Staging,   // Issuer Stage
+        Staged,    // Worker Stage
         Started,
         IssuerAbort,
         WokerAbort,
@@ -36,7 +36,6 @@ contract Order is IOrder, Ownable {
         uint startDate;
     }
 
-
     enum StageStatus {
         Init,
         Delivered,
@@ -49,10 +48,10 @@ contract Order is IOrder, Ownable {
     //交付阶段
     struct Stage {
         uint amount;        // pay amount
-        uint peroid;        // second
+        uint period;        // second
         StageStatus status;
-        string milestone;
-        string delivery;
+        string milestone;   // TODO: only as event
+        string delivery;    // TODO: only as event
     }
 
     uint8 private maxStages = 12;
@@ -61,16 +60,18 @@ contract Order is IOrder, Ownable {
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
 
-    event OrderCreated(uint indexed orderId, uint indexed taskId, address indexed issuer, address worker, address token, amount);
+    event OrderCreated(uint indexed orderId, uint indexed taskId, address indexed issuer, address worker, address token, uint amount);
     event OrderModified(uint orderId, address token, uint amount);
     event SetStage(uint indexed orderId, address worker, uint[] amounts, uint[] periods);
-    event OrderStarted(uint orderId, address who);
-    event ConfirmOrderStage(uint indexed orderId, address issuer, uint8 stageIndex);
-    event TerminateOrder(uint indexed orderId, address user);
-    event TerminateStage(uint indexed orderId, address user, uint8 stageIndex);
-    event Withdraw(uint indexed orderId, address worker, uint8 stageIndex);
+    event OrderStarted(uint taskId, uint orderId, address who);
+    event ConfirmOrderStage(uint indexed orderId, address issuer, uint stageIndex);
+    event OrderAbort(uint indexed orderId, address user, uint stageIndex);
+    event Withdraw(uint indexed orderId, address worker, uint amount);
+    event Delivery(address worker, uint orderId, uint stageIndex, string delivery);
 
     Counters.Counter private orderIds;
+
+
 
     // orderId  = > 
     mapping(uint => Order) public orders;
@@ -80,8 +81,24 @@ contract Order is IOrder, Ownable {
     // orderId = >
     mapping(uint => Stage[]) private orderStages;
 
+    bytes32 public DOMAIN_SEPARATOR;
+    bytes32 public constant PERMITSTAGE_TYPEHASH = keccak256("PermitStage(uint256 orderId,uint256[] amounts,uint256[] periods,uint256 deadline)");
+
     constructor(address _task) {
         task = _task;
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                // This should match the domain you set in your client side signing.
+                keccak256(bytes("DetaskOrder")),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
     }
 
     function createOrder(Order memory _order) external {
@@ -95,7 +112,9 @@ contract Order is IOrder, Ownable {
             worker: _order.worker,
             token: _order.token,
             amount: _order.amount,
-            progress: OrderProgess.Init
+            progress: OrderProgess.Init,
+            startDate: 0,
+            payed: 0
         });
 
         emit OrderCreated(orderId, _order.taskId, msg.sender, _order.worker, _order.token, _order.amount);
@@ -107,7 +126,7 @@ contract Order is IOrder, Ownable {
         require(msg.sender == ITask(task).ownerOf(order.taskId), "No creating permission.");
 
         if (order.payed > 0 && orders[orderId].token != token) {
-            withdraw(orderId, msg.sender, order.payed);
+            refund(orderId, msg.sender, order.payed);
         }
         orders[orderId].token = token;
         orders[orderId].amount = amount;
@@ -125,26 +144,15 @@ contract Order is IOrder, Ownable {
         if (token == address(0)) {
             order.payed += msg.value;
         } else {
-            IERC20(_token).safeTransferFrom(msg.sender, address(this), needPayAnount);
+            IERC20(token).safeTransferFrom(msg.sender, address(this), needPayAnount);
             order.payed += amount;
         }
     }
 
-    function withdraw(uint _orderId, address _to, uint _amount) public {
-        Order storage order = orders[_orderId];
-        require(msg.sender == ITask(task).ownerOf(order.taskId), "No permission.");
 
-        order.payed -= _amount;
-        doTransfer(order.token, _to, _amount);
-
-        if(order.progress >= OrderProgess.Started) {
-            require(order.payed >= order.amount, "withdraw too much");
-        }
-
-    }
 
     function setStage(uint _orderId, uint[] memory _amounts, uint[] memory _periods, string[] memory _milestones) external {
-        Order storage order = orders[orderId];
+        Order storage order = orders[_orderId];
         require(order.progress < OrderProgess.Started, "PROG_STARTED");
 
         require(order.worker == msg.sender || ITask(task).ownerOf(order.taskId) == msg.sender, "No permission.");
@@ -159,11 +167,6 @@ contract Order is IOrder, Ownable {
         emit SetStage(_orderId, msg.sender, _amounts, _periods);
     }
 
-    function appendStage(uint _orderId, uint amount, uint period, string milestone) external {
-        
-
-    }
-
     function doSetStage(uint _orderId, uint[] memory _amounts, uint[] memory _periods, string[] memory _milestones) internal {
         require(_amounts.length == _periods.length && _amounts.length != 0, "Wrong parameter length.");
         require(maxStages >= _amounts.length, "Wrong parameter length.");
@@ -176,10 +179,26 @@ contract Order is IOrder, Ownable {
                 amount: _amounts[i],
                 period: _periods[i],
                 milestone: _milestones[i],
-                status: StageStatus.Init
+                status: StageStatus.Init,
+                delivery: ""
             });
             stages.push(pro);
         }
+    }
+
+    function appendStage(uint _orderId, uint amount, uint period, string calldata milestone, bytes calldata  _signature) external {
+        
+
+    }
+
+    function prolongStage(uint _orderId, uint _stageIndex, uint newPeriod, bytes calldata _signature) external {
+        
+    }
+
+    function stageDelivery(uint _orderId, uint _stageIndex, string calldata delivery) external {
+        Stage[] storage stages = orderStages[_orderId];
+        stages[_stageIndex].delivery = delivery;
+        emit Delivery(msg.sender, _orderId, _stageIndex, delivery);
     }
 
     function checkAmount(uint orderId, Order storage order) internal {
@@ -193,16 +212,25 @@ contract Order is IOrder, Ownable {
     }
 
 
-    function permitStage(uint _orderId, uint[] memory _amounts, uint[] memory _periods, bytes memory _signature) public {
-        // TODO: 
-        // address signer = _signature.toTypedDataHash().recover(_signature);
-        
-        Order storage order = orders[_orderId];
-        address signer;
-        require(order.worker == signer, "Invalid Worker");
-        order.progress = OrderProgess.Staged;
+    function permitStage(uint _orderId, uint[] memory _amounts, uint[] memory _periods, uint deadline, bytes memory _signature) public {
+        bytes32 structHash = keccak256(abi.encode(PERMITSTAGE_TYPEHASH, _orderId, _amounts, _periods, deadline));
+        bytes32 digest = ECDSA.toTypedDataHash(DOMAIN_SEPARATOR, structHash);
 
-        //TODO: nonce
+        address recoveredAddress = ECDSA.recover(digest, _signature);
+
+        Order storage order = orders[_orderId];
+        require(order.worker == recoveredAddress, "Invalid Worker Signature");
+        
+        Stage[] storage stages = orderStages[_orderId];
+
+        require(stages.length == _periods.length && _periods.length == _amounts.length, "mismatch");
+        
+        for ( uint i = 0; i < stages.length; i++) {
+            require(_amounts[i] == stages[i].amount, "mismatch amount");
+            require(_periods[i] == stages[i].period, "mismatch period");
+        }
+        
+        order.progress = OrderProgess.Staged;
     }
 
 
@@ -225,23 +253,23 @@ contract Order is IOrder, Ownable {
             order.payed = order.amount;
         }
 
-        doStartOrder(order);
+        doStartOrder(_orderId, order);
     }
 
     function workerStartOrder(uint _orderId) external {
         Order storage order = orders[_orderId];
         uint taskId = order.taskId;
         require(order.worker == msg.sender, "No permission.");
-        require(order.progress == StageStatus.Staging, "Need Isser confirm");
+        require(order.progress == OrderProgess.Staging, "Need Issuer confirm");
         require(order.payed >= order.amount, "Need Pay");
 
         checkAmount(_orderId, order);
         
-        doStartOrder(order);
+        doStartOrder(_orderId, order);
     }
 
 
-    function doStartOrder(Order storage order) internal {
+    function doStartOrder(uint _orderId, Order storage order) internal {
         order.progress = OrderProgess.Started;
         order.startDate = block.timestamp;
 
@@ -250,23 +278,21 @@ contract Order is IOrder, Ownable {
             stages[0].status = StageStatus.Accepted;
         }
 
-        taskOrders[taskId].push(_orderId);
-        emit OrderStarted(_orderId, msg.sender);
+        taskOrders[order.taskId].push(_orderId);
+        emit OrderStarted(order.taskId, _orderId, msg.sender);
     }
 
-
-    function confirmOrderStage(uint _orderId, uint8 _stageIndex) external {
+    // confirm must continuous
+    function confirmOrderStage(uint _orderId, uint _stageIndex) external {
         uint taskId = orders[_orderId].taskId;
         require(msg.sender == ITask(task).ownerOf(taskId), "No confirming permission.");
-
         require(orderStages[_orderId][_stageIndex].status != StageStatus.Done, "Done");
 
-
         if (_stageIndex == 0) {
-            orderStages[_orderId][_stageIndex].confirmed = accepted; 
+            orderStages[_orderId][_stageIndex].status = StageStatus.Accepted;
         } else {
-            require(orderStages[_orderId][_stageIndex-1].confirmed == accepted, "The previous stage was not confirmed");
-            orderStages[_orderId][_stageIndex].confirmed = accepted; 
+            require(orderStages[_orderId][_stageIndex-1].status == StageStatus.Accepted, "The previous stage was not accepted");
+            orderStages[_orderId][_stageIndex].status = StageStatus.Accepted;
         }
         
         emit ConfirmOrderStage(_orderId, msg.sender, _stageIndex);
@@ -276,80 +302,103 @@ contract Order is IOrder, Ownable {
         return orderStages[_orderId];
     }
 
+    function getOngoingOrderStage(uint _orderId) public view returns (uint stageIndex, uint lastStageEnd) {
+        Order memory order = orders[_orderId];
+        lastStageEnd = order.startDate;
+        require(order.progress == OrderProgess.Started, "UnOngoing");
 
-    function terminateOrder(uint _orderId) external {
-        uint taskId  = orders[_orderId].taskId;
-        require(ITask(task).ownerOf(taskId) == msg.sender || orders[_orderId].worker == msg.sender, "No terminate permission.");
-        
-        
-    // enum StageStatus {
-    //     Init,
-    //     Delivered,
-    //     Accepted,
-    //     Refused,
-    //     Done       // withdrawed
-    // }
-
-
-        require(orders[_orderId].checked != IssuerChecked, "The order has been checked by issuer.");
-        
-        delete orders[_orderId];
-        delete orderStages[_orderId];
-
-        emit TerminateOrder(_orderId, msg.sender);
+        Stage[] storage stages = orderStages[_orderId];
+        uint nowTs = block.timestamp;
+        uint i = 0;
+        for (; i < stages.length; i++) {
+            Stage storage stage = stages[i];
+            if(stage.status != StageStatus.Accepted && nowTs < lastStageEnd + stage.period) {
+                stageIndex = i;
+                return (stageIndex, lastStageEnd);
+            }
+            lastStageEnd += stage.period;
+        }
+        revert("Order Ended");
     }
 
-    function terminateStage(uint _orderId, uint8 _stageIndex) external {
-        uint taskId  = orders[_orderId].taskId;
-        require(ITask(task).ownerOf(taskId) == msg.sender || orders[_orderId].worker == msg.sender, "No terminate permission.");
-        require(orderStages[_orderId][_stageIndex].confirmed != accepted, "Already confirmed.");
+    // Abort And Settle
+    function abortOrder(uint _orderId) external {
+        Order storage order = orders[_orderId];
+        address issuer = ITask(task).ownerOf(order.taskId);
+        require(order.worker == msg.sender || issuer == msg.sender, "No terminate permission.");
 
-        uint startDate = orders[_orderId].startDate;
-        uint stageStartDate;
-        uint sumAmount;
-        uint sumAmountA;
-        uint sumAmountB;
-        uint8 isConfirmed;  
+        (uint currStageIndex, uint lastStageEnd) = getOngoingOrderStage(_orderId);
+
         Stage[] storage stages = orderStages[_orderId];
-        if (_stageIndex == 0) {
-            stageStartDate = startDate;
-        } else {
-            stageStartDate = stages[_stageIndex - 1].endDate;
-            isConfirmed = stages[_stageIndex - 1].confirmed;
-        }
-        require((block.timestamp >= stageStartDate && stages[_stageIndex].endDate >= block.timestamp) || isConfirmed == accepted, "Out stage.");
-        uint period = stages[_stageIndex].endDate - stageStartDate;
-        if (block.timestamp > stageStartDate) {
-            sumAmountB += stages[_stageIndex].amount * (block.timestamp - stageStartDate) / period;
-        } else {
-            sumAmountB = 0;
+        uint forWorkerAmount;
+        uint forIssuerAmount;
+
+        for (uint i = 0; i < currStageIndex; i++) {
+            if(stages[i].status != StageStatus.Done) {
+                forWorkerAmount += stages[i].amount;
+                stages[i].status == StageStatus.Done;
+            }
         }
 
-        for (_stageIndex; _stageIndex < stages.length; _stageIndex++) {
-            sumAmount += stages[_stageIndex].amount;
-            stages[_stageIndex].confirmed == refused;
-            stages[_stageIndex].withdrawed == true;
+        Stage storage stage = stages[currStageIndex];
+        if (order.worker == msg.sender) {
+            forIssuerAmount += stage.amount;
+            order.progress = OrderProgess.WokerAbort;
+        } else {
+            forWorkerAmount += stage.amount * (block.timestamp - lastStageEnd) / stage.period;
+            forIssuerAmount += stage.amount * (lastStageEnd + stage.period - block.timestamp) / stage.period;
+            order.progress = OrderProgess.IssuerAbort;
         }
-        sumAmountA = sumAmount - sumAmountB;
-        doTransfer(orders[_orderId].token, ITask(task).ownerOf(taskId), sumAmountA);
-        doTransfer(orders[_orderId].token, orders[_orderId].worker, sumAmountB);
+        stage.status = StageStatus.Refused;
 
-        emit TerminateStage(_orderId, msg.sender, _stageIndex);
+        for (uint i = currStageIndex; i < stages.length; i++) {
+            forIssuerAmount += stages[i].amount;
+            stages[i].status == StageStatus.Refused;
+        }
+
+        doTransfer(order.token, issuer, forIssuerAmount);
+        doTransfer(order.token, order.worker, forWorkerAmount);
+
+        emit OrderAbort(_orderId, msg.sender, currStageIndex);
+    }
+
+    function refund(uint _orderId, address _to, uint _amount) public {
+        Order storage order = orders[_orderId];
+        require(msg.sender == ITask(task).ownerOf(order.taskId), "No permission.");
+
+        order.payed -= _amount;
+        doTransfer(order.token, _to, _amount);
+
+        if(order.progress >= OrderProgess.Started) {
+            require(order.payed >= order.amount, "refund too much");
+        }
     }
 
     // TODO:项目需要抽成
-    function withdraw(uint _orderId, uint8 _stageIndex) external {
-        require(orders[_orderId].worker == msg.sender, "No permission.");
-        require(orders[_orderId].checked == IssuerChecked, " Need issuer checked.");
-        Stage storage pro = orderStages[_orderId][_stageIndex];
-        require(!pro.withdrawed, "Aleady withdrawed");
+    function withdraw(uint _orderId) external {
+        Order storage order = orders[_orderId];
+        require(order.worker == msg.sender, "No permission.");
+        require(order.progress >= OrderProgess.Started, "order Unstarted");
 
-        if (pro.confirmed == accepted || pro.endDate + 7*24*60*60 < block.timestamp) {
-            doTransfer(orders[_orderId].token, msg.sender, pro.amount);
-            pro.withdrawed = true;
+
+        uint lastStageEnd = order.startDate;
+        require(order.progress == OrderProgess.Started, "UnOngoing");
+
+        Stage[] storage stages = orderStages[_orderId];
+        uint nowTs = block.timestamp;
+        uint pendingWithdraw;
+        for ( uint i = 0; i < stages.length; i++) {
+            Stage storage stage = stages[i];
+            if(stage.status == StageStatus.Accepted || nowTs >= lastStageEnd + stage.period) {
+                pendingWithdraw = stage.amount;
+                stage.status = StageStatus.Done;
+            }
+            lastStageEnd += stage.period;
         }
 
-        emit Withdraw(_orderId, msg.sender, _stageIndex);
+        doTransfer(order.token, msg.sender, pendingWithdraw);
+
+        emit Withdraw(_orderId, msg.sender, pendingWithdraw);
     }
 
     function hasTaskOrders(uint taskId) external view override  returns (bool hasTaskOrders_){
@@ -365,11 +414,11 @@ contract Order is IOrder, Ownable {
     function modifyMaxStages(uint8 _maxStages) external onlyOwner {
         maxStages = _maxStages;
     }
-    
-
 
 
     function doTransfer(address _token, address _to, uint _amount) private {
+        if (_amount == 0) return;
+
         if (address(0) == _token) {
             TransferHelper.safeTransferETH(_to, _amount);
         } else {
