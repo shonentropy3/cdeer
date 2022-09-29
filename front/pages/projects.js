33edcea18b54c54fcba253d58a0c320fb5f78ad0
cailin <cai1763921135@gmail.com>
2022-09-29 18:20:35 +0800

    en: add projects css style

--- front/pages/projects.js
+++ front/pages/projects.js
@@ -1,4 +1,4 @@
-import { Input, Empty } from 'antd';
+import { Input, Empty, Button,Pagination } from 'antd';
 import { SearchOutlined, HistoryOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
@@ -44,21 +44,21 @@ export default function Projects() {
         router.push({pathname:'/project',search: id})
     }
 
-    const searchData = () => {
-        getSearch(search)
-        .then(res => {
-            let data = res.data;
-                data.map(e => {
-                    e.role = deform_Skills(e.role);
-                    // e.task_type = deform_ProjectTypes(e.task_type);
-                })
-                projects = data;
-                setProjects([...projects]);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
+    // const searchData = () => {
+    //     getSearch(search)
+    //     .then(res => {
+    //         let data = res.data;
+    //             data.map(e => {
+    //                 e.role = deform_Skills(e.role);
+    //                 // e.task_type = deform_ProjectTypes(e.task_type);
+    //             })
+    //             projects = data;
+    //             setProjects([...projects]);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     })
+    // }
 
     const init = () => {
         tagsA = _data.skills;
@@ -88,59 +88,77 @@ export default function Projects() {
     },[selectA])
 
     return <div className="Projects">
-        <div className="search">
-            <Input placeholder="搜索项目" onChange={onChange} />
-            <SearchOutlined className="search-btn" onClick={() => searchData()} />
-        </div>
-        <div className="tags">
-            <div className="tags-list">
-                {
-                    tagsA.map((e,i) => <div 
-                                        key={i} 
-                                        className={`tags-li ${selectA === e.value ? 'tags-li-active':''}`}
-                                        onClick={() =>{selectA = e.value,setSelectA(selectA)}}
-                                        >
-                        {e.name}
-                    </div>)
-                }
+        <div className='banner'>
+            <div className='banner-content'>
+                <p className='content-title'>Looking for items</p>
+                <p className='content-subtitle'>There are skilled developers here</p>
             </div>
         </div>
-        <div className="items">
-            {
-                projects.map((e,i) => 
-                    <div key={i} className="item" onClick={() => goProject(e.id)}>
-                        <div className="info">
-                            <div className="info-img">
-                                
-                            </div>
-                            <div className="info-detail">
-                                <p className="title">{e.title}</p>
-                                <div className="tags">
-                                    技术要求: {
-                                        e.role.map((ele,index) => 
-                                            <span key={index}>{ele}</span>
-                                        )
-                                    }
-                                </div>
-                                <p className="date">项目周期: {e.period / 60 / 60 / 24}天</p>
-                            </div>
-                        </div>
-                        <div className="price">
-                            <p>项目预算: {e.budget}ETH</p>
-                            <div className="time">
-                                <HistoryOutlined className="time-icon" />
-                                3小时前发布
-                            </div>
-                        </div>
+        <div className='task-content'>
+            <div className="search">
+                <Input placeholder="搜索项目" onChange={onChange} />
+                {/* <SearchOutlined className="search-btn" onClick={() => searchData()} /> */}
+                <div className="tags">
+                    <span className='tags-keyword'>Screen</span>
+                    <div className="tags-list">
+                        {
+                            tagsA.map((e,i) => <div 
+                                                key={i} 
+                                                className={`tags-li ${selectA === e.value ? 'tags-li-active':''}`}
+                                                onClick={() =>{selectA = e.value,setSelectA(selectA)}}
+                                                >
+                                {e.name}
+                            </div>)
+                        }
                     </div>
-                )
-            }
-            {
-                projects.length === 0 ? 
+                </div>
+            </div>
+            <div className="items">
+                {
+                    projects.map((e,i) => 
+                        <div key={i} className="item" onClick={() => goProject(e.id)}>
+                            <div className="info">
+                                <div className="info-img">
+                                    
+                                </div>
+                                <div className="info-detail">
+                                    <p className="title">{e.title}</p>
+                                    <span className="time">
+                                        <HistoryOutlined className="time-icon" />
+                                        Issued 10 hours ago
+                                    </span>
+                                    <span className="date">cycle: &nbsp;{e.period / 60 / 60 / 24}天</span>
+                                    <div className="tags">
+                                        <p className='tags-title'>Recruitment type: </p>
+                                        {
+                                            e.role.map((ele,index) => 
+                                                <span className='tags-li' key={index}>{ele}</span>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                                <div className="price">
+                                    <p>
+                                        <span className='price-cost'>Cost:</span> 
+                                        <span className='price-num'>{e.budget/1000000000000000000}ETH</span>
+                                    </p>
+                                </div>
+                                <div className='item-btn'>
+                                    <Button className='btn'>Learn more</Button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+
+                {
+                    projects.length === 0 ? 
                     <Empty />
                     :
                     ''
-            }
+                }
+                <Pagination className='item-pagination' defaultCurrent={1} total={50} />
+            </div>
         </div>
     </div>
 }
