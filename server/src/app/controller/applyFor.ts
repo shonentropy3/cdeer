d34b344420cdcd5524cbf3531393068509faf1d6
Caoqizhi <2391580279@qq.com>
2022-10-11 09:13:58 +0800

    fix: server test

--- server/src/app/controller/applyFor.ts
+++ server/src/app/controller/applyFor.ts
@@ -1,45 +1,44 @@
 import { Body, Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
-import { ApplyforService } from '../service/applyfor';
+// import { ApplyforService } from '../service/applyfor';
 import { ResolutionService } from '../service/resolution';
 
 
 @Controller('applyFor')
 export class ApplyforController {
     constructor(
-        private readonly applyforService: ApplyforService,
+        // private readonly applyforService: ApplyforService,
         private readonly resolutionService: ResolutionService
     ){}
 
     @Post('applyFor')  // 报名
     async applyFor(@Body() body: any){
-        await this.applyforService.apply(body)
-        .then(() => {
-            this.resolutionService.TransHashes()
-        })
+        // await this.applyforService.apply(body)
+        // .then(() => {
+        //     this.resolutionService.TransHashes()
+        // })
     }
 
     @Post('getApply')  // 展示列表
     async getApply(@Body() body: any){
-        return await this.applyforService.getApply(body)
+        // return await this.applyforService.getApply(body)
     }
 
     @Post('cancelApply')  // 取消报名
     async cancelApply(@Body() body: any){
-        return await this.applyforService.cancel(body)
-        .then(res => {
-            this.resolutionService.TransHashes()
-            return res
-        })
+        // return await this.applyforService.cancel(body)
+        // .then(res => {
+        //     this.resolutionService.TransHashes()
+        //     return res
+        // })
     }
 
     @Post('modifyApplySwitch')  //  报名开关
     async modifyApplySwitch(@Body() body: any){
-        return await this.applyforService.modifyApplySwitch(body)
+        // return await this.applyforService.modifyApplySwitch(body)
     }
 
     @Post('modifyApplySort')  //  报名开关
     async modifyApplySort(@Body() body: any){
-        return await this.applyforService.modifyApplySort(body)
+        // return await this.applyforService.modifyApplySort(body)
     }
 }
