a0871dba2eee2b4381fc3aa88b5c175057acfa22
Caoqizhi <2391580279@qq.com>
2022-10-11 17:05:33 +0800

    del: drop_tables

--- drop_tables.sql
+++
@@ -1,7 +0,0 @@
-drop table tasks;
-drop table orders;
-drop table apply_info;
-drop table trans_hashes;
-drop table nfts;
-drop table block_logs;
-drop table users;
--- init_tables.sql
+++ init_tables.sql
@@ -1,5 +1,11 @@
 -- 创建库语句
-
+drop table tasks;
+drop table orders;
+drop table apply_info;
+drop table trans_hashes;
+drop table nfts;
+drop table block_logs;
+drop table users;
 -- 创建日志表
 CREATE TABLE block_logs (
   id serial PRIMARY KEY,
