db9c2af49d85a3c42279c06468fa8602da1a6a27
Caoqizhi <2391580279@qq.com>
2022-11-09 18:06:54 +0800

    fix: front/http  func: window

--- front/http/index.js
+++ front/http/index.js
@@ -2,39 +2,17 @@ import axios from "axios";
 import serverConfig from "./config.js";
 // import qs from "qs";
 // 创建 axios 请求实例
-const serviceAxios = axios.create({
-  baseURL: `http://${window.document.location.hostname}:3005/codemarket`, // 基础请求地址
-  timeout: 10000 // 请求超时设置
-//   withCredentials: false, // 跨域请求是否需要携带 cookie
-});
+let serviceAxios
+if (typeof window !== "undefined") {
+  const url = window.document.location.hostname;
 
-
-// 创建请求拦截
-// serviceAxios.interceptors.request.use(
-//   (config) => {
-//     // 如果开启 token 认证
-//     if (serverConfig.useTokenAuthorization) {
-//       config.headers["Authorization"] = localStorage.getItem("token"); // 请求头携带 token
-//     }
-//     // 设置请求头
-//     if(!config.headers["content-type"]) { // 如果没有设置请求头
-//       if(config.method === 'post') {
-//         config.headers["content-type"] = "application/x-www-form-urlencoded"; // post 请求
-//         config.data = qs.stringify(config.data); // 序列化,比如表单数据
-//       } else {
-//         config.headers["content-type"] = "application/json"; // 默认类型
-//       }
-//     }
-//     console.log("请求配置", config);
-//     return config;
-//   },
-//   (error) => {
-//     Promise.reject(error);
-//   }
-// );
-
-
-// 创建响应拦截
+  // Client-side-only code
+  serviceAxios = axios.create({
+    baseURL: `http://${url}:3005/codemarket`, // 基础请求地址
+    timeout: 10000 // 请求超时设置
+  //   withCredentials: false, // 跨域请求是否需要携带 cookie
+  });
+  // 创建响应拦截
 serviceAxios.interceptors.response.use(
   (res) => {
     let data = res.data;
@@ -94,4 +72,34 @@ serviceAxios.interceptors.response.use(
     return Promise.reject(message);
   }
 );
+
+}
+
+
+
+// 创建请求拦截
+// serviceAxios.interceptors.request.use(
+//   (config) => {
+//     // 如果开启 token 认证
+//     if (serverConfig.useTokenAuthorization) {
+//       config.headers["Authorization"] = localStorage.getItem("token"); // 请求头携带 token
+//     }
+//     // 设置请求头
+//     if(!config.headers["content-type"]) { // 如果没有设置请求头
+//       if(config.method === 'post') {
+//         config.headers["content-type"] = "application/x-www-form-urlencoded"; // post 请求
+//         config.data = qs.stringify(config.data); // 序列化,比如表单数据
+//       } else {
+//         config.headers["content-type"] = "application/json"; // 默认类型
+//       }
+//     }
+//     console.log("请求配置", config);
+//     return config;
+//   },
+//   (error) => {
+//     Promise.reject(error);
+//   }
+// );
+
+
 export default serviceAxios;
