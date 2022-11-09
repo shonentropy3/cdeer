77e7ee8043870e64796a8a4fda9eee8fe8bd10ca
Caoqizhi <2391580279@qq.com>
2022-11-09 17:11:06 +0800

    f

--- server/src/app/service/resolution.ts
+++ server/src/app/service/resolution.ts
@@ -22,5 +22,156 @@ export class ResolutionService {
 
     private readonly logger = new Logger(ResolutionService.name)
 
+<<<<<<< Updated upstream
     
 }
+=======
+    TransHashes = async () => {
+	console.log('begain ===>')
+        // 创建task任务
+        const log = await rpcProvider.waitForTransaction('0xf137a6ff05c115fa607a4251eb6ad5d7063c076bf69415715207be5ac892b5e1')
+        console.log(log);
+
+        return
+
+        let taskHash = await this.applyInfoRepository.query(getTaskHash());
+        for (let v of taskHash) {
+            console.log(taskHash, '<====>', v.hash);
+            
+            const log = await rpcProvider.waitForTransaction('0xf137a6ff05c115fa607a4251eb6ad5d7063c076bf69415715207be5ac892b5e1')
+            
+            console.log(log);
+            
+            const createTask = new ethers.utils.Interface(
+                ["event TaskCreated(uint256 indexed,address,tuple(string,string,string,uint8,uint112,uint32,uint48,bool))"]
+            );
+            if (!log) {
+                return
+            }
+            let decodedData = createTask.parseLog(log.logs[2]);
+            const taskId = decodedData.args[0].toString();
+            const _data = decodedData.args[2];
+
+            let params = {
+                taskId: taskId,
+                hash: v.hash,
+                title: _data[0],
+                desc: _data[1],
+                attachment: _data[2],
+                budget: _data[4].toString(),
+                period: _data[5]
+            }
+            let sql = createTaskSql(params)
+            try {
+                let sqlResult = await this.applyInfoRepository.query(sql.sql);
+                
+                if (-1 != sqlResult[1]) {
+                    await this.applyInfoRepository.query(sql.sqlUpdateTH);
+                }
+                this.logger.debug('createTasks');
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        // 创建订单
+        let createOrderHash = await this.applyInfoRepository.query(getCreateOrderHash());
+        for (const v of createOrderHash) {
+            const log = await rpcProvider.getTransactionReceipt(v.hash);
+            const createOrder = new ethers.utils.Interface([
+                "event OrderCreated(uint indexed taskId, uint indexed orderId,  address issuer, address worker, address token, uint amount)"
+            ]);
+            let decodedData = createOrder.parseLog(log.logs[0]);
+            const orderId = decodedData.args.orderId.toString();
+            const taskId = decodedData.args.taskId.toString();
+            const worker = decodedData.args.worker;
+            const issuer = decodedData.args.issuer;
+            const amount = decodedData.args.amount.toString();
+            let params = {
+                orderId: orderId,
+                taskId: taskId,
+                hash: v.hash,
+                worker: worker,
+                issuer: issuer,
+                amount: amount
+            }
+            
+            let sql = createOrderSql(params)
+            try {
+                let sqlResult = await this.applyInfoRepository.query(sql.sql);
+                if (-1 != sqlResult[1]) {
+                    await this.applyInfoRepository.query(sql.sqlUpdateTH);
+                }
+                this.logger.debug('createOrders');
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        // 报名&&修改报名
+        let applyForHash = await this.applyInfoRepository.query(getApplyForHash());
+        for (const v of applyForHash) {
+            const log = await rpcProvider.getTransactionReceipt(v.hash);
+            const ApplyFor = new ethers.utils.Interface(["event ApplyFor(uint256 indexed taskId, address indexed taker, uint256 valuation)"]);
+            let decodedData = ApplyFor.parseLog(log.logs[0]);
+            const taskId = decodedData.args.taskId.toString();
+            const taker = decodedData.args.taker;
+            const valuation = decodedData.args.valuation.toString();
+            let params = {
+                taskId: taskId,
+                applyAddr: taker,
+                valuation: valuation,
+                hash: v.hash
+            }
+            let sql = updateApplyInfo(params)
+        try {
+            let sqlBefore = await this.applyInfoRepository.query(sql.sqlBefore);
+            let sqlUpdateAI,insertAI;
+            if (sqlBefore.length > 0) {
+                sqlUpdateAI = await this.applyInfoRepository.query(sql.sqlUpdateAI);
+            } else {
+                insertAI = await this.applyInfoRepository.query(sql.insert);
+            }
+            if (-1 != sqlUpdateAI[1] || -1 != insertAI[1]) {
+                await this.applyInfoRepository.query(sql.sqlUpdateTH);
+            }
+            this.logger.debug('insertApplyFor');
+        } catch (error) {
+            console.log(error);
+        }
+        }
+
+        // 取消报名
+        let cancelApplyHash = await this.applyInfoRepository.query(getCancelApplyHash()); 
+        for (const v of cancelApplyHash) {
+            const log = await rpcProvider.getTransactionReceipt(v.hash);
+            const CancelApply = new ethers.utils.Interface(["event CancelApply(uint256 indexed taskId, address taker)"]);
+            let decodedData = CancelApply.parseLog(log.logs[0]);
+            const taskId = decodedData.args.taskId.toString();
+            const taker = decodedData.args.taker.toLowerCase();
+            let params = {
+                taskId: taskId,
+                taker: taker,
+                hash: v.hash
+            }
+            let sql = cancelApply(params)
+        try {
+            let sqlBefore = await this.applyInfoRepository.query(sql.sqlBefore);
+            let sqlDeletAI;
+            if (sqlBefore.length > 0) {
+                sqlDeletAI = await this.applyInfoRepository.query(sql.sqlDeletAI);
+                if (-1 != sqlDeletAI[1]) {
+                await this.applyInfoRepository.query(sql.sqlUpdateTH);
+            }
+            } 
+
+            this.logger.debug('insertApplyFor');
+        } catch (error) {
+            console.log(error);
+        }
+        }
+
+        // 未同步连上数据
+    }
+}
+>>>>>>> Stashed changes
