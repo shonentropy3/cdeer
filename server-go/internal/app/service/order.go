44245f0e89c163b600de76021e5759d65c74e625
MAC <1139629972@qq.com>
2022-12-07 09:09:33 +0800

    add: 业务层初始化函数

---
+++ server-go/internal/app/service/order.go
@@ -0,0 +1,10 @@
+package service
+
+// getOrderList
+// @function: getOrderList
+// @description: 分页获取个⼈进⾏中的项⽬
+// @param:
+// @return:
+func getOrderList() {
+
+}
---
+++ server-go/internal/app/service/user.go
@@ -0,0 +1,19 @@
+package service
+
+// getUserInfo
+// @function: getUserInfo
+// @description: 获取个⼈资料
+// @param:
+// @return:
+func getUserInfo() {
+
+}
+
+// updateUserInfo
+// @function: updateUserInfo
+// @description: 修改个⼈资料
+// @param:
+// @return:
+func updateUserInfo() {
+
+}
