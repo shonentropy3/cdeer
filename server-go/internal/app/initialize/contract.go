c7eedeb3eea55e9b82885d5f779710c5ba155e0b
梁文杰 <1139629972@qq.com>
2022-12-09 09:05:05 +0800

    add: 添加初始化加载合约ABI功能

---
+++ server-go/internal/app/initialize/contract.go
@@ -0,0 +1,62 @@
+package initialize
+
+import (
+	DeTaskABI "code-market-admin/abi"
+	"code-market-admin/internal/app/global"
+	"encoding/json"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type ContractAddr struct {
+	Address      string `json:"address"`
+	ContractName string `json:"contractName"`
+}
+
+// 加载合约信息
+func InitContract() {
+	// 初始化全局变量
+	global.ContractABI = make(map[string]abi.ABI)
+	global.ContractAddr = make(map[string]common.Address)
+	global.AddrContract = make(map[common.Address]string)
+	// 遍历当前目录下的JSON文件，加载ABI
+	contractAbi, err := abi.JSON(strings.NewReader(DeTaskABI.StorageMetaData.ABI))
+	if err != nil {
+		fmt.Println(err)
+	}
+	global.ContractABI["DeTask"] = contractAbi
+	// 遍历当前目录下的JSON文件，获取合约地址
+	if err := filepath.Walk(".", walkFunc); err != nil {
+		panic(err)
+	}
+}
+
+func walkFunc(path string, info os.FileInfo, err error) error {
+	// 判断文件是否是JSON文件
+	if filepath.Ext(path) == ".json" {
+		// 打开文件
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		// 解析JSON文件
+		byteValue, _ := io.ReadAll(file)
+		var contractAddr ContractAddr
+		err = json.Unmarshal([]byte(byteValue), &contractAddr)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		global.ContractAddr[contractAddr.ContractName] = common.HexToAddress(contractAddr.Address)
+		global.AddrContract[common.HexToAddress(contractAddr.Address)] = contractAddr.ContractName
+	}
+
+	return nil
+}
