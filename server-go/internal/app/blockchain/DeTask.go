05fb525e09d27ebeee301042fa4f02b3e7aaedb9
梁文杰 <1139629972@qq.com>
2022-12-12 09:43:01 +0800

    en: 修改合约事件从扫块改为解析交易哈希

--- server-go/internal/app/blockchain/DeTask.go
+++ server-go/internal/app/blockchain/DeTask.go
@@ -4,60 +4,47 @@ import (
 	DeTaskABI "code-market-admin/abi"
 	"code-market-admin/internal/app/global"
 	"code-market-admin/internal/app/model"
+	"errors"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-func DeTask(vLog types.Log) {
-	DeTaskABI := global.ContractABI["DeTask"]
-	switch vLog.Topics[0] {
-	case DeTaskABI.Events["TaskCreated"].ID:
-		ParseTaskCreated(vLog)
-	case DeTaskABI.Events["TaskModified"].ID:
-		ParseTaskModified(vLog)
-	case DeTaskABI.Events["TaskDisabled"].ID:
-		ParseTaskDisabled(vLog)
-	case DeTaskABI.Events["ApplyFor"].ID:
-		ParseApplyFor(vLog)
+func DeTask(transHash model.TransHash, Logs []*types.Log) (haveBool bool, err error) {
+	switch transHash.EventName {
+	case "TaskCreated":
+		err = ParseTaskCreated(Logs)
+		return true, err
 	}
+	return false, nil
 }
 
 // ParseTaskCreated 解析TaskCreated事件
-func ParseTaskCreated(vLog types.Log) {
-	contractAbi := global.ContractABI["DeTask"]
-	var taskCreated DeTaskABI.DeTaskTaskCreated
-	ParseErr := contractAbi.UnpackIntoInterface(&taskCreated, "TaskCreated", vLog.Data)
-	// parse success
-	if ParseErr == nil {
-		// 开始事务
-		tx := global.DB.Begin()
-		// 更新数据--不存在则插入
-		// TODO: 技能要求
-		task := model.Task{TaskID: vLog.Topics[1].Big().Uint64(), Status: 1, Title: taskCreated.Task.Title, Desc: taskCreated.Task.Desc, Period: taskCreated.Task.Period}
-		task.Budget = taskCreated.Task.Budget.String()
-		task.Attachment = taskCreated.Task.Attachment
-		task.Hash = vLog.TxHash.String()
-		task.Status = 102
-		// 更新数据
-		raw := tx.Model(&model.Task{}).Where("hash = ?", task.Hash).Updates(&task)
-		if raw.Error != nil {
-			tx.Rollback()
-			return
-		}
-		// 不存在则插入
-		if raw.RowsAffected == 0 {
-			if err := tx.Model(&model.Task{}).Save(&task); err != nil {
+func ParseTaskCreated(Logs []*types.Log) (err error) {
+	for _, vLog := range Logs {
+		contractAbi := global.ContractABI["DeTask"]
+		var taskCreated DeTaskABI.DeTaskTaskCreated
+		ParseErr := contractAbi.UnpackIntoInterface(&taskCreated, "TaskCreated", vLog.Data)
+		// parse success
+		if ParseErr == nil {
+			// 开始事务
+			tx := global.DB.Begin()
+			// 更新数据
+			task := model.Task{TaskID: vLog.Topics[1].Big().Uint64(), Status: 1, Title: taskCreated.Task.Title, Desc: taskCreated.Task.Desc, Period: taskCreated.Task.Period}
+			task.Budget = taskCreated.Task.Budget.String()
+			task.Attachment = taskCreated.Task.Attachment
+			// 更新数据
+			if err = tx.Model(&model.Task{}).Where("hash = ?", vLog.TxHash.String()).Updates(&task).Error; err != nil {
 				tx.Rollback()
-				return
+				return err
 			}
+			// 删除任务
+			if err = tx.Model(&model.TransHash{}).Where("hash = ?", vLog.TxHash.String()).Delete(&model.TransHash{}).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return tx.Commit().Error
 		}
-		// 删除任务
-		if err := tx.Model(&model.TransHash{}).Where("hash = ?", vLog.TxHash.String()).Delete(&model.TransHash{}).Error; err != nil {
-			tx.Rollback()
-			return
-		}
-		tx.Commit()
 	}
-	return
+	return errors.New("事件解析失败")
 }
 
 // ParseTaskModified 解析TaskModified事件
---
+++ server-go/internal/app/blockchain/transaction.go
@@ -0,0 +1,99 @@
+package blockchain
+
+import (
+	"code-market-admin/internal/app/global"
+	"code-market-admin/internal/app/model"
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"strings"
+	"sync"
+	"time"
+)
+
+func HandleTransaction() {
+	// 错误处理
+	defer func() {
+		if err := recover(); err != nil {
+			global.LOG.Error("HandleTransaction致命错误")
+			time.Sleep(time.Second * 3)
+			go HandleTransaction()
+		}
+	}()
+	//
+	client, err := ethclient.Dial(global.CONFIG.Contract.Provider)
+	//client, err := ethclient.Dial("https://summer-tame-pine.matic-testnet.discover.quiknode.pro/fdf3c786bf1dd3e5e848f0c98947ea4f5caee358/")
+	if err != nil {
+		panic("Error dial")
+	}
+	var txMap sync.Map
+
+	// 循环
+	for {
+		// 获取需要扫描的数据
+		var transHashList []model.TransHash
+		db := global.DB.Model(&model.TransHash{})
+		if err := db.Find(&transHashList).Error; err != nil {
+			continue
+		}
+		var haveBool bool // 是否空map
+		txMap.Range(func(key, value interface{}) bool {
+			haveBool = true
+			return false
+		})
+		// 无任务
+		if len(transHashList) == 0 && haveBool {
+			continue
+		}
+		// 任务列表
+		for _, trans := range transHashList {
+			trans.Hash = strings.TrimSpace(trans.Hash)
+			_, loaded := txMap.LoadOrStore(trans.Hash, trans)
+			if loaded == false {
+				go HandleTransactionReceipt(client, txMap, trans.Hash)
+			}
+		}
+		time.Sleep(time.Second * 10)
+	}
+}
+
+func HandleTransactionReceipt(client *ethclient.Client, txMap sync.Map, hash string) {
+	// 是否在处理列表
+	transHashAny, ok := txMap.Load(hash)
+	if !ok {
+		return
+	}
+	transHash, ok := transHashAny.(model.TransHash)
+	if !ok {
+		global.LOG.Error("HandleTransactionReceipt Reflect Error")
+		return
+	}
+	fmt.Println(hash)
+	// 解析交易Hash
+	res, err := client.TransactionReceipt(context.Background(), common.HexToHash(hash))
+	// 待交易
+	if err != nil {
+		fmt.Println("待交易")
+		fmt.Println(err)
+		txMap.Delete(hash)
+		return
+	}
+	// 交易失败
+	if res.Status == 0 {
+		fmt.Println("交易失败")
+		HandleTraverseFailed(transHash)
+		txMap.Delete(hash)
+	}
+	// 交易成功
+	if res.Status == 1 {
+		fmt.Println("交易成功")
+		if err := HandleTraverseSuccess(transHash, res.Logs); err != nil {
+			fmt.Println(err)
+			txMap.Delete(hash)
+		} else {
+			fmt.Println("交易成功--删除")
+			txMap.Delete(hash)
+		}
+	}
+}
