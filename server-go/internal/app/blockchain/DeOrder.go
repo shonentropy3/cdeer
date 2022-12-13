5d9bffcbf5d8177ee08ed2309e65fc6160ea114c
梁文杰 <1139629972@qq.com>
2022-12-13 15:09:19 +0800

    en: server-go 优化合约事件解析

--- server-go/internal/app/blockchain/DeOrder.go
+++ server-go/internal/app/blockchain/DeOrder.go
@@ -1,13 +1,49 @@
 package blockchain
 
 import (
+	DeTaskABI "code-market-admin/abi"
 	"code-market-admin/internal/app/global"
+	"code-market-admin/internal/app/model"
+	"errors"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-func DeOrder(vLog types.Log) {
-	DeOrderABI := global.ContractABI["DeOrder"]
-	switch vLog.Topics[0] {
-	case DeOrderABI.Events["OrderCreated"].ID:
+func DeOrde(transHash model.TransHash, Logs []*types.Log) (haveBool bool, err error) {
+	switch transHash.EventName {
+	case "OrderCreated":
+		err = ParseOrderCreated(Logs)
+		return true, err
 	}
+
+	return false, nil
+}
+
+// ParseOrderCreated 解析OrderCreated事件
+func ParseOrderCreated(Logs []*types.Log) (err error) {
+	contractAbi := global.ContractABI["DeOrder"]
+	for _, vLog := range Logs {
+		var taskCreated DeTaskABI.DeTaskTaskCreated
+		ParseErr := contractAbi.UnpackIntoInterface(&taskCreated, "TaskCreated", vLog.Data)
+		// parse success
+		if ParseErr == nil {
+			// 开始事务
+			tx := global.DB.Begin()
+			// 更新数据
+			task := model.Task{TaskID: vLog.Topics[1].Big().Uint64(), Show: true, Status: 102, Title: taskCreated.Task.Title, Desc: taskCreated.Task.Desc, Period: taskCreated.Task.Period}
+			task.Budget = taskCreated.Task.Budget.String()
+			task.Attachment = taskCreated.Task.Attachment
+			// 更新数据
+			if err = tx.Model(&model.Task{}).Where("hash = ?", vLog.TxHash.String()).Updates(&task).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			// 删除任务
+			if err = tx.Model(&model.TransHash{}).Where("hash = ?", vLog.TxHash.String()).Delete(&model.TransHash{}).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return tx.Commit().Error
+		}
+	}
+	return errors.New("事件解析失败")
 }
