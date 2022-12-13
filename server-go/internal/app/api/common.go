0aacb34295400bacb30109f248932ac6b4fa9695
梁文杰 <1139629972@qq.com>
2022-12-13 15:08:11 +0800

    add: server-go 新增上传文件到本地功能

--- server-go/internal/app/api/common.go
+++ server-go/internal/app/api/common.go
@@ -6,6 +6,8 @@ import (
 	"code-market-admin/internal/app/service"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"path"
+	"strings"
 )
 
 func Upload(c *gin.Context) {
@@ -34,4 +36,34 @@ func Upload(c *gin.Context) {
 		}
 		response.OkWithDetailed(response.UploadResponse{Hash: hash}, "上传成功", c)
 	}
+}
+
+// UploadImage
+// @Tags CommonApi
+// @Summary 上传图片文件
+// @Router /common/uploadImage [post]
+func UploadImage(c *gin.Context) {
+	_, header, err := c.Request.FormFile("file")
+	if err != nil {
+		global.LOG.Error("接收文件失败!", zap.Error(err))
+		response.FailWithMessage("接收文件失败", c)
+		return
+	}
+	// 读取文件后缀
+	ext := path.Ext(header.Filename)
+	ext = strings.ToLower(ext)
+	// 限制文件后缀
+	if (ext == ".jpg" || ext == ".png" || ext == ".jpeg" || ext == ".gif") == false {
+		global.LOG.Error("文件格式不正确", zap.String("ext", ext))
+		response.FailWithMessage("文件格式不正确", c)
+		return
+	}
+
+	file, err := service.UploadImage(header) // 文件上传后拿到文件路径
+	if err != nil {
+		global.LOG.Error("修改数据库链接失败!", zap.Error(err))
+		response.FailWithMessage("修改数据库链接失败", c)
+		return
+	}
+	response.OkWithDetailed(file, "上传成功", c)
 }
