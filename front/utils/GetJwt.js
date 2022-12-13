c0030102cf4246688bc94d0a432cb2e88db255be
Caoqizhi <2391580279@qq.com>
2022-12-13 15:45:08 +0800

    en: /front/GetJwt jwt解析添加错误处理

--- front/utils/GetJwt.js
+++ front/utils/GetJwt.js
@@ -1,6 +1,11 @@
 export function getJwt(token) {
     let strings = token.split("."); //截取token，获取载体
-    var userinfo = JSON.parse(decodeURIComponent(escape(window.atob(strings[1].replace(/-/g, "+").replace(/_/g, "/"))))); //解析，需要吧‘_’,'-'进行转换否则会无法解析
+    try {
+        var userinfo = JSON.parse(decodeURIComponent(escape(window.atob(strings[1].replace(/-/g, "+").replace(/_/g, "/"))))); //解析，需要吧‘_’,'-'进行转换否则会无法解析
+    } catch (error) {
+        localStorage.clear();
+        history.go(0);
+    }
 
     return userinfo
 }
