f705305580ffccb9e2c7c9fd9dfc61d9984583fe
Apple <apple@AppledeMac-mini.local>
2022-12-13 11:29:16 +0800

    en:优化user表role字段

--- server-go/internal/app/model/task_role_relate.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type TaskRoleRelate struct {
-	ID     uint `gorm:"column:id;primarykey" json:"id" form:"id"`
-	TaskID uint `gorm:"column:task_id" json:"task_id" form:"task_id"`
-	RoleID uint `gorm:"column:role_id" json:"role_id" form:"role_id"`
-}
