5b745526f80dca21f8d859bfda9ba17b4934d344
xilibi2003@163.com <xilibi2003@163.com>
2022-12-19 18:23:29 +0800

    add builder and issuer sbt

---
+++ contracts/BuilderSBT.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./interface/IOrder.sol";
+import "./SBTBase.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BuilderSBT is SBTBase, Ownable {
+
+    error InvalidCaller();
+
+    address public deOrder;
+
+    event SetDeorder(address deorder);
+
+    constructor(address _order) SBTBase("DeTask Builder", "Builder") {
+        deOrder = _order; 
+        emit SetDeorder(_order);
+    }
+
+
+    modifier onlyDeorder() {
+        if(msg.sender != deOrder) revert InvalidCaller(); 
+        _;
+    }
+
+    function mint(uint orderId) external onlyDeorder {
+        Order memory order = IOrder(deOrder).getOrder(orderId);
+        _mint(order.worker, orderId);
+    }
+
+
+    function setDeOrder(address _deOrder) external onlyOwner {
+        deOrder = _deOrder;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return "";
+    }
+}
