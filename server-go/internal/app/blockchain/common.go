b4eacdd3ebcbd56252ce358c87292150e043da00
梁文杰 <1139629972@qq.com>
2022-12-28 18:00:41 +0800

    en: server-go 修改技能转换

--- server-go/internal/app/blockchain/common.go
+++ server-go/internal/app/blockchain/common.go
@@ -5,18 +5,18 @@ import (
 	"strconv"
 )
 
-var currencyNames = map[uint8]string{1: "USD", 2: "ETH", 3: "BNB"}
+var currencyNames = map[uint8]string{1: "USD", 2: "ETH", 3: "BNB", 4: "MATIC", 5: "OP"}
 
 func ParseSkills(skills int64) (res pq.Int64Array) {
 	skillsBin := strconv.FormatInt(skills, 2)
 	var skillsList pq.Int64Array
-	for i := 0; i <= len(skillsBin)/8; i++ {
-		if i == len(skillsBin)/8 {
-			skillInt, _ := strconv.ParseInt(skillsBin[:len(skillsBin)-8*i], 2, 64)
-			skillsList = append(skillsList, skillInt+100)
+	for i := 0; i <= len(skillsBin)/12; i++ {
+		if i == len(skillsBin)/12 {
+			skillInt, _ := strconv.ParseInt(skillsBin[:len(skillsBin)-12*i], 2, 64)
+			skillsList = append(skillsList, skillInt)
 		} else {
-			skillInt, _ := strconv.ParseInt(skillsBin[len(skillsBin)-8*(i+1):len(skillsBin)-8*i], 2, 64)
-			skillsList = append(skillsList, skillInt+100)
+			skillInt, _ := strconv.ParseInt(skillsBin[len(skillsBin)-12*(i+1):len(skillsBin)-12*i], 2, 64)
+			skillsList = append(skillsList, skillInt)
 		}
 	}
 	return skillsList
