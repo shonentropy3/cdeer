1bba8106ac395eb8ebe9c5d1c7944967c0bf33b5
Caoqizhi <2391580279@qq.com>
2022-12-29 09:40:43 +0800

    del: /front 删除部分注释

--- front/utils/BitOperation.js
+++ front/utils/BitOperation.js
@@ -1,12 +1,8 @@
-import BigNumber from 'bignumber.js';
-
 export const BitOperation = (arr) => {
     // 移位运算 << 12
     let num = 0;
     arr.forEach((ele,index) => {
-        // num += ele << ( arr.length - (index + 1) ) * 12 
         num += ele * Math.pow(2, ( arr.length - (index + 1) ) * 12)
-        console.log(ele * Math.pow(2, ( arr.length - (index + 1) ) * 12));
     })
     return num
 }
