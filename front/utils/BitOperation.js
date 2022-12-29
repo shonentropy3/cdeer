20ebc2efcecc5d49677077ffb84da7585b9131d2
Caoqizhi <2391580279@qq.com>
2022-12-29 09:37:19 +0800

    en: /front/BitOperation 修改移位运算部分 完成36位移位

--- front/utils/BitOperation.js
+++ front/utils/BitOperation.js
@@ -4,9 +4,9 @@ export const BitOperation = (arr) => {
     // 移位运算 << 12
     let num = 0;
     arr.forEach((ele,index) => {
-        console.log(ele << ( ( arr.length - new BigNumber((index + 1) ) * 12 )));
-        // num.plus(ele << ( ( arr.length - (index + 1) ) * 12 ))
-        num += ele << ( ( arr.length - new BigNumber((index + 1) ) * 12 ))
+        // num += ele << ( arr.length - (index + 1) ) * 12 
+        num += ele * Math.pow(2, ( arr.length - (index + 1) ) * 12)
+        console.log(ele * Math.pow(2, ( arr.length - (index + 1) ) * 12));
     })
     return num
 }
