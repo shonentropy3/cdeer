885f963d0ed1cc0bae7a706a4fbc930e054dff3a
梁文杰 <1139629972@qq.com>
2023-01-09 14:01:42 +0800

    add: server-go 使用embed静态化合约地址文件

--- server-go/internal/app/initialize/contract.go
+++ server-go/internal/app/initialize/contract.go
@@ -1,12 +1,13 @@
 package initialize
 
 import (
+	"code-market-admin/abi"
 	"code-market-admin/internal/app/global"
 	"encoding/json"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"io"
-	"os"
+	"io/fs"
 	"path/filepath"
 )
 
@@ -20,17 +21,19 @@ func InitContract() {
 	// 初始化全局变量
 	global.ContractAddr = make(map[string]common.Address)
 	// 遍历当前目录下的JSON文件，获取合约地址
-	dir := fmt.Sprintf("./abi/%s/", global.CONFIG.Contract.Net)
-	if err := filepath.Walk(dir, walkFunc); err != nil {
-		panic(err)
+	for _, v := range global.CONFIG.BlockChain {
+		if err := fs.WalkDir(abi.ABIFS, v.Name, walkFunc); err != nil {
+			panic(err)
+		}
 	}
 }
 
-func walkFunc(path string, info os.FileInfo, err error) error {
+func walkFunc(path string, info fs.DirEntry, err error) error {
+	dir := filepath.Dir(path) // 目录名称
 	// 判断文件是否是JSON文件
-	if filepath.Ext(path) == ".json" {
+	if !info.IsDir() && filepath.Ext(path) == ".json" {
 		// 打开文件
-		file, err := os.Open(path)
+		file, err := abi.ABIFS.Open(path)
 		if err != nil {
 			return err
 		}
@@ -39,12 +42,12 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 		// 解析JSON文件
 		byteValue, _ := io.ReadAll(file)
 		var contractAddr ContractAddr
-		err = json.Unmarshal([]byte(byteValue), &contractAddr)
+		err = json.Unmarshal(byteValue, &contractAddr)
 		if err != nil {
 			fmt.Println(err)
 		}
-
-		global.ContractAddr[contractAddr.ContractName] = common.HexToAddress(contractAddr.Address)
+		mapName := dir + ":" + contractAddr.ContractName
+		global.ContractAddr[mapName] = common.HexToAddress(contractAddr.Address)
 	}
 
 	return nil
